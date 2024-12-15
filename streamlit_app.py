7c0dec0e749179864a6586834865b5f2d9e556c1
SpRobotTeam <116852444+SpRobotTeam@users.noreply.github.com>
2024-12-15 22:37:58 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,22 +4,19 @@ from speed_analysis import speed_analysis
 from gantt_chart import gantt_chart
 from gomoku_module import gomoku_game
 from AmphibiousTrainDevelopment import display_amphibious_train_project
-from posting import posting
-
-
+from posting import posting, initialize_posts
 
 # 페이지 설정
 st.set_page_config(page_title="분석 도구 및 오목 게임", layout="wide")
-
 
 # 사이드바 메뉴
 st.sidebar.title("도구 선택")
 analysis_type = st.sidebar.radio(
     "분석 유형을 선택하세요:",
     (
-        "3D 선형성 평가", 
-        "속도 및 가속도 분석", 
-        "프로젝트 진행 간트 차트", 
+        "3D 선형성 평가",
+        "속도 및 가속도 분석",
+        "프로젝트 진행 간트 차트",
         "오목 게임",
         "수륙 양용 기차",
         "게시판"  # 게시판 메뉴 추가
@@ -27,6 +24,9 @@ analysis_type = st.sidebar.radio(
 )
 
 # 각 기능 실행
+initialize_posts()  # 상태 초기화
+
+
 if analysis_type == "3D 선형성 평가":
     linearity_analysis()
 elif analysis_type == "속도 및 가속도 분석":
